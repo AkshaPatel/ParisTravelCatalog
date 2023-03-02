591309e6ecd96a4d1dad7493ad76fc0b3ca1c2ff
Stanley Fung <stanley_fung@Stanleys-MacBook-Air.local>
2023-03-02 18:42:16 -0500

    Changes to Cuisine webpage

--- paris_travel_catalog/src/Components/Cuisine.js
+++ paris_travel_catalog/src/Components/Cuisine.js
@@ -1,8 +1,46 @@
 
 function Cuisine() {
     return (
-        <div>  
+        <div className= "main">
+
+            <div className= "cuisine-header"> 
             <h1> Cuisine </h1>
+            <h4> Experience the these various cuisines when visiting Paris, France!</h4>
+            </div>
+
+            <div className = "sandwich">
+                <img src = {} alt = "Croque Monsieur" />
+                <p>
+                    The Croque Monsieur is a sandwich made with ham, cheese, butter, and a special sauce called Bechamel. Its origns trace back to 1910, being found at a bistro in Paris. 
+                The cheese is put on the outside and inside of the sandwich and is baked until the cheese has melted into each other. Either Gruyère, Emmental or Comte would be the types of cheese that would be put into the Croque Monsieur. 
+                The Bechamel is a white sauce that is made of milk, fat, and flour and enhances the flavor of the sandwich. 
+                The bread is usually a bit sweeter than your normal white bread and is sometimes dipped in egg before being baked. All the individual aspects of the dish being combined makes this a flavor like no other!
+                This staple Parisian dish is popular during brunch or lunch and is typically served with a salad and can be found at bistros within Paris. 
+                </p>
+            </div>
+
+            <div className = "beef-stew">
+                <img src = {} alt = "Boeuf Bourguignon" />
+                <p>
+                    The Boeuf Bourguignon is French beef stew, that consists of Burgundy red wine, beef stock, onions, mushrooms, bouquet garni, and lardons.
+                The origins of this dish was first doucmented in 1867 and was considered "low-class", due to being made with leftover meat. It became popular during the 20th century as a Burgundian dish.
+                This dish is usually slow-cooked, sometime even taking over eight hours to create. The addition of the Burgundy red wine separates Beef Bourguignon from your typical beef stew.
+                The flavors in this dish can be described as rich, with its various vegetables and seasonings, along with the high quality beef that is found inside the dish! Potatoes are also served inside the stew or on the side of the dish.
+                This dish can be found across various restaurants and bistros across Paris. 
+                    
+
+                </p>
+            </div>
+
+            <div className = "dessert">
+                <img src ={} alt "Créme Brulée" />
+                <p>
+                    The Créme Brulée is a dessert that contains vanilla custard that is topped with caramelized sugar. 
+                The origins of this dessert can been seen within French cookbooks in the late 17th century, before being popularized in the 1980's.
+                The top of the dessert is left warm from the caramelization, while the center of the desert is kept ice cold. This dessert is sometimes topped off with various fruits. 
+                This dessert can be found at almost all places that serve dessert within Paris. 
+                </p>
+            </div>
         </div>
     );
 }
