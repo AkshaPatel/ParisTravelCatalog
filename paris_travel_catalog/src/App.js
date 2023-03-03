e8458c12004ea05645015d23c33b466483edcf47
AkshaPatel <akshpatel564@gmail.com>
2023-03-03 12:10:43 -0500

    made a separate file for Navbar

--- paris_travel_catalog/src/App.js
+++ paris_travel_catalog/src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 
-import About from './Components/About';
-import Activities from './Components/Activities';
-import Attractions from './Components/Attractions';
-import Cuisine from './Components/Cuisine';
-import InvalidURL from './Components/InvalidURL';
-
-import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 function App() {
   return (
@@ -14,46 +8,8 @@ function App() {
       <div>
       <header> Paris Travel Catalog </header>
       </div>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout/>}>
-              <Route index element={<About/>}/>
-              <Route path="activities" element={<Activities/>}/>
-              <Route path="attractions" element={<Attractions/>}/>
-              <Route path="cuisine" element={<Cuisine/>}/>
-              <Route path="*" element={<InvalidURL/>}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <Navbar />
     </>
-  );
-}
-
-const linkStyle = {textDecoration: "none", color: "white"};
-function Layout() {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link style={linkStyle} to='/'>Home</Link>
-          </li>
-          <li>
-            <Link style={linkStyle} to='activities'>Activities</Link>
-          </li>
-          <li>
-            <Link style={linkStyle} to='attractions'>Attractions</Link>
-          </li>
-          <li>
-            <Link style={linkStyle} to='cuisine'>Cuisine</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet/>
-    </>    
   );
 }
 
