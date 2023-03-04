c6557f0a9a8096d788faced6831308de5d673558
Stanley Fung <stanley_fung@Stanleys-MacBook-Air.local>
2023-03-03 23:13:18 -0500

    Finalized Cuisine webpage

--- paris_travel_catalog/src/Components/Cuisine.js
+++ paris_travel_catalog/src/Components/Cuisine.js
@@ -18,7 +18,7 @@ function Cuisine() {
                 <h1> Croque Monsieur </h1>
                 <img src = {croqueMonsieur} alt = "Croque Monsieur" />
                 <p>
-                    The Croque Monsieur is a sandwich made with ham, cheese, butter, and a special sauce called Bechamel. Its origns trace back to 1910, being found at a bistro in Paris. 
+                    The Croque Monsieur is a sandwich made with ham, cheese, butter, and a special sauce called Bechamel. Its origins trace back to 1910, being found at a bistro in Paris. 
                 The cheese is put on the outside and inside of the sandwich and is baked until the cheese has melted into each other. Either Gruyère, Emmental or Comte would be the types of cheese that would be put into the Croque Monsieur. 
                 The Bechamel is a white sauce that is made of milk, fat, and flour and enhances the flavor of the sandwich. 
                 The bread is usually a bit sweeter than your normal white bread and is sometimes dipped in egg before being baked. All the individual aspects of the dish being combined makes this a flavor like no other!
@@ -31,7 +31,7 @@ function Cuisine() {
                 <img src = {beefBourguigon} alt = "Boeuf Bourguignon" />
                 <p>
                     The Boeuf Bourguignon is French beef stew, that consists of Burgundy red wine, beef stock, onions, mushrooms, bouquet garni, and lardons.
-                The origins of this dish was first doucmented in 1867 and was considered "low-class", due to being made with leftover meat. It became popular during the 20th century as a Burgundian dish.
+                The origins of this dish was first documented in 1867 and was considered "low-class", due to being made with leftover meat. It became popular during the 20th century as a Burgundian dish.
                 This dish is usually slow-cooked, sometime even taking over eight hours to create. The addition of the Burgundy red wine separates Beef Bourguignon from your typical beef stew.
                 The flavors in this dish can be described as rich, with its various vegetables and seasonings, along with the high quality beef that is found inside the dish! Potatoes are also served inside the stew or on the side of the dish.
                 This dish can be found across various restaurants and bistros across Paris. 
