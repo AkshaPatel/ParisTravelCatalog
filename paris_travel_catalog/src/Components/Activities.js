cba75ede8d77ea2ec105af8de4fed5e4d2a7dc85
manav809 <manavpatel6978@gmail.com>
2023-03-05 12:48:20 -0500

    90% done

--- paris_travel_catalog/src/Components/Activities.js
+++ paris_travel_catalog/src/Components/Activities.js
@@ -1,16 +1,38 @@
 import "./activities.css"
+import disneyland from './images/disney.webp'
+import cruise from './images/cruise.jpeg'
+import baguette from './images/baguette.jpeg'
 function Activities() {
     return (
-        <div>
+        <div className="main">
             <div className="firstsection">
-                <p>Hello</p> 
-            </div>    
+                
+                <div className="text-box">
+                    <img src={disneyland} alt=""/>
+                    <h2>Disney Land: Paris</h2>
+                    <p>
+                    </p>
+                </div>
+            </div> 
+
             <div className="secondsection">
-                <p>Hello</p>
+                <div className="text-box">    
+                    <img src={cruise} alt="" />
+                    <h2>Paris River Cruise</h2>
+                    <p>
+                    </p>
+                </div>
             </div>
+
             <div className="thirdsection">
-                <p>Hello</p>
+                <div className="text-box">
+                    <img src={baguette} alt="" className="baguetteimg" width="350" height="250"/>
+                    <h2>Baguette Making Class</h2>
+                    <p>
+                    </p>
+                </div>
             </div>
+
         </div>
     );
 }
