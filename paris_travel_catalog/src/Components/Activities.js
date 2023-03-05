f93d917cdc14f349ccf536768f2f2c1e20f9c855
manav809 <manavpatel6978@gmail.com>
2023-03-05 13:08:44 -0500

    writing

--- paris_travel_catalog/src/Components/Activities.js
+++ paris_travel_catalog/src/Components/Activities.js
@@ -10,7 +10,12 @@ function Activities() {
                 <div className="text-box">
                     <img src={disneyland} alt=""/>
                     <h2>Disney Land: Paris</h2>
-                    <p>
+                    <p>Not only does Paris have world famous bread and great scenic views, 
+                        it is home to another disneyland. For those who are traveling 
+                        to Paris, going to Disneyland is a great way to not only 
+                        explore Paris but also Disney, which is known worldwide. An activity 
+                        that captivates people is the Star Wars Hyperspace Mountain 
+                        that has Star Wars related activities such as amusement and rides. 
                     </p>
                 </div>
             </div> 
@@ -19,7 +24,13 @@ function Activities() {
                 <div className="text-box">    
                     <img src={cruise} alt="" />
                     <h2>Paris River Cruise</h2>
-                    <p>
+                    <p>Along with eating a baguette by the Eiffel Tower, how about 
+                        eating a baguette on a boat cruise while enjoying nightfall 
+                        in Paris. This is a great scenic activity to do with loved ones 
+                        and even by yourself. This boat cruise encapsulated everything 
+                        Paris has to offer in one cruise ride. In the cruise enjoy great 
+                        French delicacies and enjoy the atmosphere it offers. 
+
                     </p>
                 </div>
             </div>
@@ -28,7 +39,12 @@ function Activities() {
                 <div className="text-box">
                     <img src={baguette} alt="" className="baguetteimg" width="350" height="250"/>
                     <h2>Baguette Making Class</h2>
-                    <p>
+                    <p> Why eat baguettes when you could learn to make a baguette in Paris 
+                        itself. Book a 2 hour baguette making course in the heart of Paris. 
+                        This is a great way to learn the history behind the incredible and 
+                        world renowned food. It is also a great way to connect with tourists and 
+                        French locals who share similar passions as yourself. 
+
                     </p>
                 </div>
             </div>
